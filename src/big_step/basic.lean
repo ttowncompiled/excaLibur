e2a5120fe21ef91f245ca90db470144ee0ded221
Ian Riley <ian.riley.915@gmail.com>
2020-09-04 23:31:21 -0500

    feat(big_step) : adds lemmas with proofs

--- src/big_step/basic.lean
+++ src/big_step/basic.lean
@@ -22,3 +22,210 @@ inductive big_step : (stmt × scope) → scope → Prop
     big_step (stmt.while b S, s) s
 
 infix ` ⟹ `:110 := big_step
+
+namespace big_step
+
+@[simp] lemma skip_iff {s t : scope} : (stmt.skip, s) ⟹ t ↔ t = s :=
+begin
+    apply iff.intro,
+    {
+        intro h₁,
+        cases h₁,
+        refl,
+    },
+    {
+        intro h₂,
+        rw h₂,
+        exact big_step.skip,
+    }
+end
+
+@[simp] lemma assign_iff {x : string} {a : scope → Prop} {s t : scope} :
+    (stmt.assign x a, s) ⟹ t ↔ t = (s{x ↦ a s}) :=
+begin
+    apply iff.intro,
+    {
+        intro h₁,
+        cases h₁,
+        refl,
+    },
+    {
+        intro h₂,
+        rw h₂,
+        exact big_step.assign,
+    }
+end
+
+@[simp] lemma seq_iff {S T : stmt} {s t : scope} :
+    (S ;; T, s) ⟹ t ↔ (∃ (u : scope), (S, s) ⟹ u ∧ (T, u) ⟹ t) :=
+begin
+    apply iff.intro,
+    {
+        intro h₁,
+        cases h₁,
+        apply exists.intro,
+        apply and.intro; assumption
+    },
+    {
+        intro h₂,
+        cases h₂,
+        cases h₂_h,
+        apply big_step.seq; assumption
+    }
+end
+
+@[simp] lemma ite_iff {b : scope → Prop} {S T : stmt} {s t : scope} :
+    (stmt.ite b S T, s) ⟹ t ↔ (b s ∧ (S, s) ⟹ t) ∨ (¬ b s ∧ (T, s) ⟹ t) :=
+begin
+    apply iff.intro,
+    {
+        intro h₁,
+        cases h₁,
+        {
+            apply or.intro_left,
+            cc,
+        },
+        {
+            apply or.intro_right,
+            cc,
+        }
+    },
+    {
+        intro h₂,
+        cases h₂,
+        {
+            cases h₂,
+            apply big_step.ite_true; assumption
+        },
+        {
+            cases h₂,
+            apply big_step.ite_false; assumption
+        }
+    }
+end
+
+@[simp] lemma ite_true_iff {b : scope → Prop} {S T : stmt} {s t : scope}
+    (hcond : b s) : (stmt.ite b S T, s) ⟹ t ↔ (S, s) ⟹ t :=
+begin
+    apply iff.intro,
+    {
+        intro h₁,
+        cases h₁,
+        {
+            exact h₁_hbody,
+        },
+        {
+            exfalso,
+            apply h₁_hcond; assumption
+        }
+    },
+    {
+        intro h₂,
+        apply big_step.ite_true; assumption
+    }
+end
+
+@[simp] lemma ite_false_iff {b : scope → Prop} {S T : stmt} {s t : scope}
+    (hcond : ¬ b s) : (stmt.ite b S T, s) ⟹ t ↔ (T, s) ⟹ t :=
+begin
+    apply iff.intro,
+    {
+        intro h₁,
+        cases h₁,
+        {
+            exfalso,
+            apply hcond; assumption
+        },
+        {
+            exact h₁_hbody,
+        }
+    },
+    {
+        intro h₂,
+        apply big_step.ite_false; assumption
+    }
+end
+
+lemma while_iff {b : scope → Prop} {S : stmt} {s u : scope} :
+    (stmt.while b S, s) ⟹ u ↔ (∃ (t : scope), b s ∧ (S, s) ⟹ t
+        ∧ (stmt.while b S, t) ⟹ u) ∨ (¬ b s ∧ u = s) :=
+begin
+    apply iff.intro,
+    {
+        intro h₁,
+        cases h₁,
+        {
+            apply or.intro_left,
+            apply exists.intro h₁_t,
+            cc
+        },
+        {
+            apply or.intro_right,
+            cc,
+        }
+    },
+    {
+        intro h₂,
+        cases h₂,
+        case or.inl {
+            cases h₂ with t h₂,
+            cases h₂ with hb h₂,
+            cases h₂ with hS hwhile,
+            exact big_step.while_true hb hS hwhile,
+        },
+        case or.inr {
+            cases h₂ with hb h₂,
+            rw h₂,
+            apply big_step.while_false; assumption
+        }
+    }
+end
+
+lemma while_true_iff {b : scope → Prop} {S : stmt} {s u : scope}
+    (hcond : b s) : (stmt.while b S, s) ⟹ u ↔
+        (∃ (t : scope), (S, s) ⟹ t ∧ (stmt.while b S, t) ⟹ u) :=
+begin
+    apply iff.intro,
+    {
+        intro h₁,
+        cases h₁,
+        {
+            apply exists.intro h₁_t,
+            cc,
+        },
+        {
+            exfalso,
+            apply h₁_hcond; assumption
+        }
+    },
+    {
+        intro h₂,
+        cases h₂ with t h₂,
+        cases h₂ with hS hwhile,
+        apply big_step.while_true hcond hS hwhile,
+    }
+end
+
+@[simp] lemma while_false_iff {b : scope → Prop} {S : stmt} {s t:  scope}
+    (hcond : ¬ b s) : (stmt.while b S, s) ⟹ t ↔ t = s :=
+begin
+    apply iff.intro,
+    {
+        intro h₁,
+        cases h₁,
+        {
+            exfalso,
+            apply hcond; assumption
+        },
+        {
+            refl,
+        }
+    },
+    {
+        intro h₂,
+        rw h₂,
+        apply big_step.while_false; assumption
+    }
+end
+
+end big_step
