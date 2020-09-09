0432d7f3ae6d95e9ae91fdebf734a1526a4c1a82
Ian Riley <ian.riley.915@gmail.com>
2020-09-09 15:31:06 -0500

    chore(*) : add notation for stmt

--- src/common/stmt.lean
+++ src/common/stmt.lean
@@ -14,4 +14,14 @@ inductive stmt : Type
 | call      : string → (scope → Prop) → (scope → Prop) →
                 (scope → scope) → stmt → stmt
 
+notation ` _skip_ ` := stmt.skip
+
+notation ` _let_ ` x ` := ` a := stmt.assign x a
+
 infixr ` ;; `:90 := stmt.comp
+
+notation ` _if_ ` b ` _then_ ` S ` _else_ ` T ` _end_ ` := stmt.ite b S T
+
+notation ` _while_ ` b ` _do_ ` S ` _end_ ` := stmt.while b S
+
+notation ` _call_ ` f ` _with_ ` `(` v₀ `, ` v₁ `)` ` | ` σ ` _do_ ` T ` _end_ ` := stmt.call f v₀ v₁ σ T
