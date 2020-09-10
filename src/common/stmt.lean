535548bd52b860e744a7ba30b85bc00cf16ce640
Ian Riley <ian.riley.915@gmail.com>
2020-09-10 12:34:26 -0500

    fix(*) : use mapsto notation for bindings

--- src/common/stmt.lean
+++ src/common/stmt.lean
@@ -20,8 +20,8 @@ notation ` _skip_ ` := stmt.skip
 This notation can be read as, "let the propositional variable for x be bound
 to the proposition a."
 -/
-notation ` _let_ ` x ` ⇒ ` a `, ` := stmt.assign x (λ (s : scope), a) -- ⇒ \=>
-notation ` _let_ ` x ` => ` a `, ` := stmt.assign x (λ (s : scope), a)
+notation ` _let_ ` x ` ↦ ` a `, ` := stmt.assign x (λ (s : scope), a)
+notation ` _let_ ` x ` |-> ` a `, ` := stmt.assign x (λ (s : scope), a)
 
 infixr ` ;; `:90 := stmt.comp
 
@@ -34,7 +34,7 @@ notation ` _while_ ` b ` _do_ ` `{ ` S ` } ` := stmt.while b S
 This notation can be read as, "call procedure f with bindings v₀ and bindings
 v₁ scoped through σ, which executes statement T."
 -/
-notation ` _call_ ` f ` _with_ ` `(` n₀ ` ⇒ `:110 v₀ `, ` n₁ ` ⇒ `:110 v₁ `)` ` |↦| ` σ ` _do_ ` `{ ` T ` } ` :=
+notation ` _call_ ` f ` _with_ ` `(` n₀ ` ↦ `:110 v₀ `, ` n₁ ` ↦ `:110 v₁ `)` ` |↦| ` σ ` _do_ ` `{ ` T ` } ` :=
 stmt.call f (λ (s : scope), s n₀ → v₀) (λ (s : scope), s n₁ → v₁) σ T
-notation ` _call_ ` f ` _with_ ` `(` n₀ ` => `:110 v₀ `, ` n₁ ` => `:110 v₁ `)` ` |->| ` σ ` _do_ ` `{ ` T ` } ` :=
+notation ` _call_ ` f ` _with_ ` `(` n₀ ` |-> `:110 v₀ `, ` n₁ ` |-> `:110 v₁ `)` ` ||->| ` σ ` _do_ ` `{ ` T ` } ` :=
 stmt.call f (λ (s : scope), s n₀ → v₀) (λ (s : scope), s n₁ → v₁) σ T
